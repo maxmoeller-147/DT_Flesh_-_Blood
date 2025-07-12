30dbdfb7e551f28791e20e69550f8bfb42eb4f13
Max Moeller <maximomoeller@gmail.com>
2025-07-13 04:06:25 +1000

    Types Table added

--- flesh_and_blood_db.sql
+++ flesh_and_blood_db.sql
@@ -1,4 +1,18 @@
+
+DROP TABLE IF Colours
+DROP TABLE IF Types
+
+
+
+
+
 CREATE TABLE Colours (
     Colour_id INT PRIMARY KEY,
-    ColourName VARCHAR(50) NOT NULL
+    ColourName VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE Types (
+    Type_id INT PRIMARY KEY,
+    TypeName VARCHAR(100) NOT NULL
 );
