fe504c51319170722d2c1b2de242f056eedc7b2a
Max Moeller <maximomoeller@gmail.com>
2025-07-13 04:19:33 +1000

    Cards TAble added

--- flesh_and_blood_db.sql
+++ flesh_and_blood_db.sql
@@ -1,6 +1,8 @@
 
-DROP TABLE IF Colours
+DROP TABLE IF Cards
+DROP TABLE IF Classes
 DROP TABLE IF Types
+DROP TABLE IF Colours
 
 
 
@@ -21,4 +23,14 @@ CREATE TABLE Types (
 CREATE TABLE Classes (
     Class_id INT PRIMARY KEY,
     ClassName VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE Cards (
+    Card_id INT PRIMARY KEY,
+    CardName VARCHAR(100) NOT NULL,
+    Type_id INT NOT NULL,
+    Colour_id INT NOT NULL,
+    FOREIGN KEY (Colour_id) REFERENCES Colours(Colour_id) ON DELETE CASCADE,
+    FOREIGN KEY (Type_id) REFERENCES Types(Type_id) ON DELETE CASCADE
 );
